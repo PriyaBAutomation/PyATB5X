9b84cae167f6792bb1aa2a58904c7c9419aa7e98
VAIJANATH <pawalkon614@gmail.com>
2024-11-07 08:32:45 +0530

    5nov

---
+++ Nov/exa_051124_python_basics/lab001_python_helloworld.py
@@ -0,0 +1,7 @@
+print("Hello World", end='___________')
+print(2 + 3)
+print(2 * 3)
+print(6 / 10)
+#print(*values: object,sep: str | None = " ",end: str | None = "\n", file: SupportsWrite[str] | None = None, flush: Literal[False] = False) -> None
+print("Hello World", "Priya", sep='*********', end='___________')
+print("Hi, how is going")
