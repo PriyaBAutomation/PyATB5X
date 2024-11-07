02049271a58adfd4d75d2bb027eedcbf290331ab
VAIJANATH <pawalkon614@gmail.com>
2024-11-07 08:32:45 +0530

    5nov

---
+++ Nov/exa_051124_python_basics/lab001_python_helloworld.py
@@ -0,0 +1,9 @@
+#To print the text,numbers
+print("Hello World")
+print(2 + 3, 3 - 2)
+print(2 * 3)
+print(10 / 4)
+#To print by modifying the default separator(1 space) and end (shifts to new line) argument
+#print(*values: object,sep: str | None = " ",end: str | None = "\n", file: SupportsWrite[str] | None = None, flush: Literal[False] = False) -> None
+print("Hello World", "Priya", sep='*********', end='___________')
+print("Hi, how is going")
---
+++ Nov/exa_091124/lab003.py
@@ -0,0 +1,21 @@
+# Program for addition, subtraction, multiplication and division of the numbers taken from user inputs
+n1= input("Enter 1st number: ")
+n2= input("Enter 2nd number: ")
+n3= input("Enter 3rd number: ")
+n1= int(n1)
+n2 = int(n2)
+n3 = int(n3)
+add = n1 + n2 + n3
+add = int(add)
+print("Addition is:",add)
+subtract = n1 - n2
+print("subtraction is :",subtract)
+multiply = n1 * n2 * n3
+print("Multiplication is :",multiply)
+# Program to find quotient and remainder of numbers taken from user inputs
+divide = int(n1 / n2)
+int(divide)
+remainder = n1 % n2
+print("Division is :",divide)
+print("Remainder is :",remainder)
+
