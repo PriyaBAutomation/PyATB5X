8be5188d73bf0e2bf8a410424c5ba2aea070cd3f
VAIJANATH <pawalkon614@gmail.com>
2024-11-15 22:02:05 +0530

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	Nov/exa_051124_python_basics/lab001_python_helloworld.py

---
+++ Nov/exa_121124/lab004_if else.py
@@ -0,0 +1,12 @@
+print(type(a))
+
+if a == 10:
+
+    print("Hello World")
+
+else:
+
+    print("Not Hello")
+
+    #NameError: name 'a' is not defined
+
---
+++ Nov/exa_121124/lab006_if_else_maxNo.py
@@ -0,0 +1,8 @@
+#Problem to find the max between two ( 3,4) → 4
+num1 = input('Enter a number1: ')
+num2 = input('Enter a number2: ')
+
+if num1 >= num2:
+    print("The maximum number is", num1)
+else :
+    print("The maximum number is", num2)
